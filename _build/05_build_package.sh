2d0f48a0eaeb402b646b708fb45d3a3b3edd2b89
ChloeXu <xuzhuqing1124@gmail.com>
2019-12-08 20:47:43 -0500

    Latest

--- _build/05_build_package.sh
+++ _build/05_build_package.sh
@@ -8,7 +8,11 @@ mkdir build
 cd build
 
 echo "Copying files..."
+cp -R ../package.json ./
 cp -R ../dist/* ./
+# optional only if not using layer
+cp -R ../node_modules ./
+cp -R ../appspec.yml ./
 
 echo "Zipping files..."
 zip -r ../sb-status-service.zip .
